1e960933e8cd7d3d89f489a53039fba5a88e5a91
jursko90 <skok.jure@gmail.com>
2026-09-21 14:37:56 +0200

    refactor: use current config entry platform helpers

--- custom_components/mojelektro/__init__.py
+++ custom_components/mojelektro/__init__.py
@@ -1,50 +1,27 @@
-from homeassistant.core import HomeAssistant
+"""Moj Elektro integration."""
+
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.helpers.event import async_track_time_interval
-from homeassistant.helpers import entity_registry as er
+from homeassistant.const import Platform
+from homeassistant.core import HomeAssistant
 from homeassistant.helpers import config_validation as cv
+
 from .const import DOMAIN
-import logging
 
-_LOGGER = logging.getLogger(__name__)
-
-# Define CONFIG_SCHEMA as config entry only
 CONFIG_SCHEMA = cv.config_entry_only_config_schema(DOMAIN)
+PLATFORMS = [Platform.SENSOR]
 
-async def async_setup(hass: HomeAssistant, config: dict):
-    """Set up the integration via configuration.yaml (if applicable)."""
+
+async def async_setup(hass: HomeAssistant, config: dict) -> bool:
+    """Set up the integration domain."""
     return True
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
-    """Set up the integration from a config entry."""
-    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = entry.data
 
-    # Migrate existing entities to be grouped under a single device
-    await migrate_existing_entities_to_device(hass, entry)
-
-    # Forward the setup to sensor platform
-    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
-    
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up Moj Elektro from a config entry."""
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
-    """Handle removal of an entry."""
-    unload_ok = await hass.config_entries.async_forward_entry_unload(entry, "sensor")
-    if unload_ok:
-        hass.data[DOMAIN].pop(entry.entry_id)
-    return unload_ok
 
-async def migrate_existing_entities_to_device(hass: HomeAssistant, entry: ConfigEntry):
-    """Migrate old entities without devices to a single device."""
-    entity_registry = er.async_get(hass)
-    meter_id = entry.data.get("meter_id")
-    
-    _LOGGER.info(f"Entity migration started for meter_id {meter_id}.")
-
-    # Find all entities for this integration
-    for entity_entry in er.async_entries_for_config_entry(entity_registry, entry.entry_id):
-        # If the entity doesn't have a device_id, we'll assign it to the new device
-        if entity_entry.device_id is None:
-            _LOGGER.debug(f"Migrating entity {entity_entry.entity_id} to new device.")
-    
-    _LOGGER.debug(f"Entity migration completed for integration {DOMAIN}.")
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a Moj Elektro config entry."""
+    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
